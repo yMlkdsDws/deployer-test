5951c4aa0ebc72acb55d68c2e06706fbee2a6166
ii masamitsu <ii@entap.co.jp>
2021-09-03 19:34:11 +0900

    prettier

---
+++ app/Services/Settings/AdminSuspendUserUseCase.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Services\Settings;
+
+class AdminSuspendUserUseCase
+{
+    /**
+     * ユーザーを凍結する
+     */
+    public function suspendUser(string $userId)
+    {
+    }
+}
---
+++ app/Services/Settings/AdminUnsuspendUserUseCase.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Services\Settings;
+
+class AdminUnsuspendUserUseCase
+{
+    /**
+     * ユーザーの凍結を解除する
+     */
+    public function unsuspendUser(string $userId)
+    {
+    }
+}
