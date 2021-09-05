ab89b787bdc530d38bcba87e0c3d2b4f69d64d24
ii masamitsu <ii@entap.co.jp>
2021-09-05 19:15:56 +0900

    autocompleteが効かないのを修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -42,7 +42,7 @@
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
-                                name="password_confirmation" required />
+                                name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
